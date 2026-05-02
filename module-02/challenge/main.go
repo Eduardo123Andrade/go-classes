508fbe9cebee52c5685e131da3b14f3dc1650e9c
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2026-05-02 13:18:16 +0000

    feat: save data from file on memory

--- module-02/challenge/main.go
+++ module-02/challenge/main.go
@@ -4,11 +4,11 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 type Measurement struct {
-	Nome  string
 	Min   float64
 	Max   float64
 	Sum   float64
@@ -23,15 +23,37 @@ func main() {
 	}
 	defer measurement.Close()
 
+	dados := make(map[string]Measurement)
+
 	scanner := bufio.NewScanner(measurement)
 
 	for scanner.Scan() {
 		rawData := scanner.Text()
 		semicolonIndex := strings.Index(rawData, ";")
 		location := rawData[:semicolonIndex]
-		temperature := rawData[semicolonIndex+1:]
+		rawTemperature := rawData[semicolonIndex+1:]
 
-		fmt.Println(location, temperature)
+		temperature, _ := strconv.ParseFloat(rawTemperature, 64)
+		measurement, ok := dados[location]
 
+		if !ok {
+			measurement = Measurement{
+				Min:   temperature,
+				Max:   temperature,
+				Sum:   temperature,
+				Count: 1,
+			}
+		} else {
+			measurement.Min = min(measurement.Min, temperature)
+			measurement.Max = max(measurement.Min, temperature)
+			measurement.Sum += temperature
+			measurement.Count++
+		}
+
+		dados[location] = measurement
+	}
+
+	for name, measmeasurement := range dados {
+		fmt.Printf("%s: %#+v\n", name, measmeasurement)
 	}
 }
