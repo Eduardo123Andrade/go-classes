7be006df3e4f264dbb8dd136d2ef2cc6dca282d3
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2026-05-02 20:27:01 -0300

    feat: read file test

--- file-module/main.go
+++ file-module/main.go
@@ -6,39 +6,20 @@ import (
 )
 
 func main() {
-	// file, err := os.OpenFile("files/file.txt", os.O_WRONLY, 0644)
 	file, err := os.OpenFile("files/file.txt", os.O_WRONLY, 0644)
 	if err != nil {
 		panic(err)
 	}
 
-	defer file.Close()
-
-	// stats, err := file.Stat()
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// stats
-
-	// err = os.WriteFile(file.Name(), []byte("hello, world2"), 0644)
-
-	// result, err := file.Re("testes")
-
-	r, err := file.Write([]byte("hello, world"))
-	r, err = file.Write([]byte("\n"))
-	r, err = file.Write([]byte("hello, world2"))
-	r, err = file.Write([]byte("\n"))
+	stats, err := file.Stat()
 
 	if err != nil {
-		panic(err)	
+		panic(err)
 	}
 
-	// if err == nil {
-	// 	fmt.Println("File written successfully")
-	// }
+	mode := stats.Mode()
 
-	fmt.Println(r)
-
-
+	fmt.Println(mode)
+	fmt.Println(mode.Type())
+	fmt.Printf("Binary(mode): %b\n", mode)
 }
